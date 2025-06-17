c837f3f27fd978a7e71809fbb0f4521e41894b03
Aries-Surya <ariessurya8124@gmail.com>
2025-06-17 18:38:22 +0530

    Update Angular CLI version, modify about component with new content, and enhance styling in CSS files

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -5,8 +5,21 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
 })
 export class AboutComponent {
+  birthDate: Date = new Date('2003-10-11');
 
+  get age(): number {
+    const today = new Date();
+    let age = today.getFullYear() - this.birthDate.getFullYear();
+    const monthDifference = today.getMonth() - this.birthDate.getMonth();
+    if (
+      monthDifference < 0 ||
+      (monthDifference === 0 && today.getDate() < this.birthDate.getDate())
+    ) {
+      age--;
+    }
+    return age;
+  }
 }
